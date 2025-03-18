5357a824735524a484d2769d6a58c9f45b7ce948
Felix <flxowens@pm.me>
2025-03-18 19:19:59 +0000

    feat: implement logging initialisation using the tracing crate

--- src/utils/logging.rs
+++ src/utils/logging.rs
@@ -1,4 +1,27 @@
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+/// Initialise logging
+///
+/// This function sets up the logging system using the `tracing` crate.
+/// It creates a subscriber that formats log messages using the `fmt`
+/// formatter and sets the global default subscriber.
+///
+/// # Example
+///
+/// ```rust
+/// use semantic_gallery::utils::logging;
+///
+/// fn main() {
+///     logging::init();
+/// }
+/// ```
 pub fn init() {
-    // TODO: Initialize logging
-    todo!("Initialize logging with tracing")
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+
+    info!("Logging initialised");
 }
